2ede8720530b3fbf1a8379d65b44bfa2ccfaecc6
NageshIngole <inagesh51@gmail.com>
2024-06-23 21:35:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World By React</h1>
+       
+    </>
+  )
+}
+
+export default App
